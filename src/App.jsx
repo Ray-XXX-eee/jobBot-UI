676e0c29cbd4742bbb683fb36952377a1183a63b
Raihan Kabir <raihanscareer@gmail.com>
2024-02-05 02:41:16 +0000

    implemented github pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import Dashboard from "./pages/Dashboard/components/Dashboard";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Registration/components/Register";
-import { RouterProvider, createBrowserRouter, createHashRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import PostList from "./pages/Dashboard/components/PostList";
 import CreatePost from "./pages/Dashboard/components/CreatePost";
 import Test from "./test/Test";
@@ -11,43 +11,49 @@ import PrivateRoute from "./pages/PrivateRoute";
 import ContextProvider from "./store/AppContext";
 import SeekerSettings from "./pages/Dashboard/components/SeekerSettings";
 
-const router = createHashRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/register",
+      element: (
+        <ContextProvider>
+          <Register />
+        </ContextProvider>
+      ),
+    },
+    { path: "/test", element: <Test /> },
+    {
+      path: "/login",
+      element: (
+        <ContextProvider>
+          <Login />
+        </ContextProvider>
+      ),
+    },
+    {
+      path: "/",
+      element: (
+        <ContextProvider>
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        </ContextProvider>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <PostList />,
+        },
+        { path: "/post-job", element: <CreatePost /> },
+        { path: "/seeker-settings", element: <SeekerSettings /> },
+      ],
+    },
+  ],
   {
-    path: "/register",
-    element: (
-      <ContextProvider>
-        <Register />
-      </ContextProvider>
-    ),
-  },
-  { path: "/test", element: <Test /> },
-  {
-    path: "/login",
-    element: (
-      <ContextProvider>
-        <Login />
-      </ContextProvider>
-    ),
-  },
-  {
-    path: "/",
-    element: (
-      <ContextProvider>
-        <PrivateRoute>
-          <Dashboard />
-        </PrivateRoute>
-      </ContextProvider>
-    ),
-    children: [
-      {
-        path: "/",
-        element: <PostList />,
-      },
-      { path: "/post-job", element: <CreatePost /> },
-      { path: "/seeker-settings", element: <SeekerSettings /> },
-    ],
-  },
-]);
+    // Add the basename here
+    basename: "/jobBot-UI",
+  }
+);
 
 function App() {
   return <RouterProvider router={router} />;
